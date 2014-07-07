9abe0213ec84af1c97f02050e255eea45d101940
Rian Neufelt <RNeufelt@live.ca>
2014-07-07 03:17:14 -0700

    completed first problem

---
+++ ProjectEuler/1/MultiplesOf3And5.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ProjectEuler._1
+{
+    public class MultiplesOf3And5
+    {
+        public static int Run(int max)
+        {
+            int sum = 0;
+            for (int i = 0; i < max; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
